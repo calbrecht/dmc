bbc9760bd02923e00c4443cd749bf7eef2172670
Christian Albrecht <christian.albrecht@mayflower.de>
2015-03-13 08:36:08 +0100

    Start project

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject dmc "0.1.0-SNAPSHOT"
+  :description "Upnp media controller with web interface"
+  :url "https://github.com/calbrecht/dmc"
+  :license {:name "Eclipse Public License - v 1.0"
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  )
